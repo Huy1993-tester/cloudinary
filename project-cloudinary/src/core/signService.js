b2d4126fa6d9c5f350e28983e5e4bafa0c903dc7
Nguyen Cong Tri Dung <nguyencongtridung@gmail.com>
2021-12-11 02:05:16 +0700

    - fix call api

--- project-cloudinary/src/core/signService.js
+++ project-cloudinary/src/core/signService.js
@@ -5,7 +5,7 @@ class SignService {
     axios({
       url: "http://localhost:8000/api/ca/sign-xml/111660364373410324602044888586771781661",
       method: "POST",
-      body: {
+      data: {
         xml_url:
           "https://res.cloudinary.com/vianey/raw/upload/v1638261951/ca/drivers_tzdixv.xml",
       },
@@ -15,7 +15,7 @@ class SignService {
     axios({
       url: "http://127.0.0.1:8000/api/ca/sign-xml/111660364373410324602044888586771781661",
       method: "POST",
-      body: {
+      data: {
         "xml_url": "https://res.cloudinary.com/vianey/raw/upload/v1638261951/ca/drivers_tzdixv.xml"
     }
     });
